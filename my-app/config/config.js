c1915cb3c0a6e3f9102bda59fff6debf1ccc9a53
Liam R Zadoorian <41171966+XC4Life101@users.noreply.github.com>
2025-02-21 19:30:19 +0000

    Sequelize to render db connection test

---
+++ my-app/config/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    development: {
+      username: "ryan",
+      password: "IgVjTq4UkUBXlj5cWCmZ1jzIogVgJ92l",
+      database: "mydb_u88s",
+      host: "dpg-cur2tb2j1k6c73co7kkg-a",
+      dialect: "postgres",
+    },
+    production: {
+      use_env_variable: "dpg-cur2tb2j1k6c73co7kkg-a.oregon-postgres.render.com",
+      dialect: "postgres",
+    },
+  };
+  
