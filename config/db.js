80219bcdc97015c522418811301b81f5dc6e5bcf
Luke Yen <luksogami@icloud.com>
2025-03-12 12:20:32 -0700

    connectiion and created table

--- config/db.js
+++
@@ -1,8 +0,0 @@
-import Sequelize from 'sequelize'
-const sequelize = new Sequelize('postgres://postgres:test123@10.1.10.237:5432/postgres') // Example for postgres
-try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
---
+++ server.js
@@ -0,0 +1,35 @@
+import Sequelize from 'sequelize'
+const sequelize = new Sequelize('postgres://postgres:test123@10.1.10.237:5432/postgres') // Example for postgres
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+  import { DataTypes } from 'sequelize';
+  
+  const User = sequelize.define(
+    'User',
+    {
+      // Model attributes are defined here
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        // allowNull defaults to true
+      },
+    },
+    {
+      // Other model options go here
+    },
+  );
+User.sync()
+const jane = await User.create({ firstName: 'Jane', lastName:'zadoorian' });
+// Jane exists in the database now!
+console.log(jane instanceof User); // true
+console.log(jane.firstname); // "Jane"
+  
+  // `sequelize.define` also returns the model
+  console.log(User === sequelize.models.User); // true
